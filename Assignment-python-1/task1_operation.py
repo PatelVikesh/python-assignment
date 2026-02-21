65b83f344f5eba2397d55d1be0d560a811ef9856
Vikesh patel <kumarvikesh8182@gmail.com>
2026-02-21 22:23:07 +0530

    Add files via upload

---
+++ Assignment-python-1/task1_operation.py
@@ -0,0 +1,11 @@
+# Task 1 : Perform basic mathematical operation
+
+# Taking input from user
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+
+#doing calculation
+print("addition is:", num1+num2)
+print("subtraction is:", num1-num2)
+print("multiplication is:", num1*num2)
+print("division is:", num1/num2)
---
+++ Assignment-python-1/task2_operation.py
@@ -0,0 +1,9 @@
+# taking name from user
+first_name = input("enter your first name: ")
+last_name = input("enter your last name: ")
+
+#joining names
+full_name = first_name + " " + last_name
+
+#printing personalized message
+print("hello, "+ full_name + "! welcome to the python program." )
